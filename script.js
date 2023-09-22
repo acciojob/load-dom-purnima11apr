6cb924c5a14a5d3b10136fff628f8733d0f0a566
purnima11apr <141928600+purnima11apr@users.noreply.github.com>
2023-09-22 16:42:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+ document.addEventListener('DOMContentLoaded', function () {
+
+	 var bodyElement = document.body;
+
+	  bodyElement.innerHTML = '';
+
+	 var textNode = document.createTextNode('DOM load success');
+
+	  bodyElement.appendChild(textNode);
+        });
